b2468f7d6c034e260737f8b607283d6a6f5dc4bc
Prasanth <prasanth@techserw.com>
2025-09-26 18:47:39 +0530

    ADDED: Original theme colors

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -4,19 +4,19 @@ const lightTheme = createTheme({
   palette: {
     mode: "light",
     primary: {
-      main: "#0A66C1",
+      main: "#341010",
       contrastText: "#f8f8f6"
     },
     secondary: {
-      main: "#f8f8f6"
+      main: "#D3AF37"
     },
     background: {
       default: "#ffffff",
       paper: "#f8f8f6"
     },
     text: {
-      primary: '#28282b',
-      secondary: '#6c6e6d'
+      primary: '#341010',
+      secondary: '#D3AF37'
     }
   },
   typography: {
